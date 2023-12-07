6ef58ef47565b0e21b941bc126522da6ce84e4f9
Prashareet <prashareet@gmail.com>
2023-12-07 12:55:33 +0530

    Added contexts and components with functionality

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,25 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
+import { ThemeContextProvider } from './contexts/Themes'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = ()=>{
+    setThemeMode("light")
+  }
+  const darkTheme = ()=>{
+    setThemeMode("dark")
+  }
+  
+  useEffect(()=>{
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode])
+  
   return (
     <>
-      
-      <div className="flex flex-wrap min-h-screen items-center">
-          <div className="w-full">
-              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                  
-              </div>
+      <ThemeContextProvider value={{themeMode, lightTheme, darkTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                    <ThemeBtn/>
+                </div>
 
-              <div className="w-full max-w-sm mx-auto">
-                  
-              </div>
-          </div>
-      </div>
-
+                <div className="w-full max-w-sm mx-auto">
+                    <Card/>
+                </div>
+            </div>
+        </div>
+      </ThemeContextProvider>
     </>
   )
 }
